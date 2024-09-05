948a695626705fd3e64d87f480593b60a9bea664
rony-hassan <rony.cse@gmail.com>
2024-09-05 20:31:25 +0600

    added some random code

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-// 05-09-2024
-
-/*basic starts here*/
-
-console.log(`Hello World`);
-console.log(`I am Rony`);
-
-//window.alert(`This is an alert message`);
-//window.alert(`Message from Rony`);
-
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I am Rony`;
-
-/* basic ends here */
-
-/*variables starts here*/
-
-let x = 123;
-
-console.log(x);
-
+let fullName = "Rony Hassan";
 let age = 25;
-let price = 10.99;
-let gpa = 3.67;
-let firstName = "Rony";
+let student = false;
 
-//console.log("Your age is ${age} years old"); Not Working
-console.log(`Your age is ${age} years old`);
-console.log(`The price is $${price}`);
-console.log(`Your gpa is ${gpa}`);
-
-console.log(typeof "age");
-console.log(typeof(age));
-console.log(typeof age); //To get the datatype
-
-console.log(firstName);
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
