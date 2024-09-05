007dc8d87311f36bc23392418408ca323142b560
rony-hassan <rony.cse@gmail.com>
2024-09-05 20:04:45 +0600

    added basic code

---
+++ script.js
@@ -0,0 +1,36 @@
+// 05-09-2024
+
+/*basic starts here*/
+
+console.log(`Hello World`);
+console.log(`I am Rony`);
+
+//window.alert(`This is an alert message`);
+//window.alert(`Message from Rony`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I am Rony`;
+
+/* basic ends here */
+
+/*variables starts here*/
+
+let x = 123;
+
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 3.67;
+let firstName = "Rony";
+
+//console.log("Your age is ${age} years old"); Not Working
+console.log(`Your age is ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is ${gpa}`);
+
+console.log(typeof "age");
+console.log(typeof(age));
+console.log(typeof age); //To get the datatype
+
+console.log(firstName);
