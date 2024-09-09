c3e35a968a289ecb7b38f782896df4723ec50a33
rony-hassan <rony.cse@gmail.com>
2024-09-09 11:24:21 +0600

    added Math object

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// counter program
+// Math = built-in object that provides a collection of properties and methods
 
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
+let x =3;
+let y =2;
+let z =1;
 
-let count = 0;
+//z = Math.round(x);
+//z= Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x,y);
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.abs(x);
 
-decreaseBtn.onclick = function() {
-    count--;
-    countLabel.textContent = count;
-}
+let max = Math.max(x,y,z);
+console.log(max);
 
-increaseBtn.onclick = function() {
-    count++;
-    countLabel.textContent = count;
-}
-
-resetBtn.onclick = function() {
-    count = 0;
-    countLabel.textContent = count;
-}
+let min = Math.min(x,y,z);
+console.log(min);
