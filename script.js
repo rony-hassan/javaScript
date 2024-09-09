492369cfc1c346ce89f58a8e5c817e835e072b65
rony-hassan <rony.cse@gmail.com>
2024-09-09 12:01:22 +0600

    solved random number generator

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-// Math = built-in object that provides a collection of properties and methods
+// Random number generator
 
-let x =3;
-let y =2;
-let z =1;
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
 
-//z = Math.round(x);
-//z= Math.floor(x);
-//z = Math.ceil(x);
-//z = Math.trunc(x);
-//z = Math.pow(x,y);
-//z = Math.sqrt(x);
-//z = Math.log(x);
-//z = Math.sin(x);
-//z = Math.abs(x);
+const min =1;
+const max=6;
 
-let max = Math.max(x,y,z);
-console.log(max);
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-let min = Math.min(x,y,z);
-console.log(min);
+myButton.onclick = function() {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
