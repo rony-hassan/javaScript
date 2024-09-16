3dae671e44e313f40c52e9eb4be96b26229921cf
rony-hassan <rony.cse@gmail.com>
2024-09-16 21:26:33 +0600

    function added

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-// Number Guessing Game
+//function = A section of reusable code.
+//            Declare code once, use it whenever you want.
+//            Call the function to execute that code.
 
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+function isEven(number) {
+    return number % 2 ===0 ? true : false;
+}
 
-let attempts = 0;
-let guess;
-let running = true;
-
-while(running){
-
-    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}`);
-    guess = Number(guess);
-
-    if(isNaN(guess)) {
-        window.alert("Please enter a valid number");
-    }else if(guess < minNum || guess > maxNum){
-        window.alert(`Please enter a number between ${minNum} and ${maxNum}`);
+function isValidEmail(email){
+    if(email.includes('@')){
+        return true;
     }
-    else{
-        attempts++;
-        if(guess < answer) {
-            window.alert("Too low! Try again.");
-        }
-        else if(guess > answer) {
-            window.alert("Too high! Try again.");
-        }
-        else{
-            window.alert(`Congratulations! You guessed the correct number in ${attempts} attempts.`);
-            running = false;
-        }
+    else {
+        return false;
     }
 }
+
+console.log(isEven(1086));
+console.log(isValidEmail("emal@gmail.com"));
