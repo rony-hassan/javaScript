b6201bc13447d7d2bd31fa38c58c9c482bf0e8e1
rony-hassan <rony.cse@gmail.com>
2024-09-30 16:47:17 +0600

    Random Password Generator added

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-// Dice Roller
+// Random Password Generator
 
-function rollDice() {
-    const numOfDice = document.getElementById("numOfDice").value;
-    const diceResult = document.getElementById("diceResult");
-    const diceImages = document.getElementById("diceImages");
-    const values = [];
-    const images = [];
+function generatePassword(length,includeLowercase,includeUppercase,includeNumbers,includeSymbols){
 
-    for(let i=0;i<numOfDice;i++){
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src= "images/${value}.png" alt = "Dice ${value}">`);
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length <=0){
+        return `(password length must be at least 1)`;
     }
 
-    diceResult.textContent = `Dice: ${values.join(', ')}`;
-    diceImages.innerHTML = images.join('');
+    if(allowedChars === 0){
+        return `(At least 1 set of character needs to be selected)`;
+    }
+
+    for(let i=0; i<length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
 }
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength,includeLowercase,includeUppercase,includeNumbers,includeSymbols);
+
+console.log(`Generate password: ${password}`);
