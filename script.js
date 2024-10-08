39b153b099528f747182b1c9a7185bd6ae39213f
rony-hassan <rony.cse@gmail.com>
2024-10-08 18:53:53 +0600

    Lecture 43 added

--- script.js
+++ script.js
@@ -1,47 +1,8 @@
-/* Lecture 42
-    destructuring = extract values from arrays and objects, then
-                    assign them to variables in a convenient way
-                    [] = to perform array destructuring
-                    {} = to perfrom object destructuring
+/* Lecture 43
+    nested objects = Objects inside of other Objects.
+                    Allows you to represent more complex data structures
+                    Child Object is enclosed by a Parent Object
+
+                    person{Address{}, ContactInfo{}}
+                    ShoppingCart{Keyboard{},Mouse{},Monitor{}}
  */
-
-//-----Example 1 -------
-// SWAP the value of two variables
-
-let a = 1;
-let b = 2;
-
-[a,b] = [b,a];
-
-console.log(a);
-console.log(b);
-
-//-----Example 2 -------
-// SWAP 2 elements in an array
-
-const colors = ["red","green","blue","black","white"];
-
-[colors[0], colors[4]] = [colors[4], colors[0]];
-console.log(colors);
-
-//-----Example 3 -------
-// Assign array elements to variables
-
-const color = ["red","green","blue","black","white"];
-const [firstcolor, secondcolor, thirdcolor] = color;
-console.log(firstcolor);
-console.log(thirdcolor);
-
-//-----Example 4 -------
-// Extract values from objects
-
-const person1 = {
-    firstName : "Bob",
-    lastName : "Square",
-    age : 30,
-    job : "Cook"
-}
-
-const {firstName,lastName,age,job} = person1;
-
-console.log(firstName);
