93ce84475cb4c8cca53c84bfa66f3bcbe12e12d6
rony-hassan <rony.cse@gmail.com>
2024-11-02 23:22:03 +0600

    project completed

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-/* Lecture 43
-    nested objects = Objects inside of other Objects.
-                    Allows you to represent more complex data structures
-                    Child Object is enclosed by a Parent Object
+// Lecture 44
+// Shuffle an array using Fisher-Yates Algorithm
 
-                    person{Address{}, ContactInfo{}}
-                    ShoppingCart{Keyboard{},Mouse{},Monitor{}}
- */
+const cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+
+shuffle(cards);
+
+console.log(cards);
+
+function shuffle(array){
+    for(let i=array.length - 1; i>0;i--){
+        const random = Math.floor(Math.random() * (i+1));
+
+        [array[i],array[random]] = [array[random],array[i]];
+    }
+}
